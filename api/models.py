df5b30761226cc546d51067c89d9428df2b407af
sachin barali <baralisachin5925@gmail.com>
2024-08-30 23:32:07 +0545

    get request and path for images of rentlist

--- api/models.py
+++ api/models.py
@@ -7,11 +7,19 @@ class Category(models.Model):
 
     def __str__(self):
         return self.categoryName
+
+
+class Image(models.Model):
+    image = models.ImageField(null=True, blank=True, upload_to='images/')
+
+    def __str__(self):
+        return str(self.image)
+
     
 class RentList(models.Model):
     name = models.CharField(max_length=200,null=True, blank=True)
     price = models.DecimalField(max_digits=7,decimal_places=2,null=True, blank=True)
-    # images
+    images = models.ManyToManyField(Image)
     description= models.TextField(null=True,blank=True)
     rating= models.DecimalField(max_digits=7,decimal_places=2, null=True, blank=True)
     numReviews= models.IntegerField(null=True, blank=True, default=0)
--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from api import views
 urlpatterns = [
     path('',views.index),
     path('categories/', views.CategoryView.as_view(), name= "category"),
+    path('rentlist/',views.RentListView.as_view(), name="rentlist"),
 ]
