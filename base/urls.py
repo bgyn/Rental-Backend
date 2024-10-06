48dd024888eded751c275c9d49692e859d5f5917
sachin barali <baralisachin5925@gmail.com>
2024-10-06 21:47:51 +0545

    changes:url-changes
    url changes for rentlist item

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('categories/',views.CategoryView.as_view(),name="categories"),
     path('rules/',views.RuleView.as_view(),name="rule"),
-    path('rentitem/',views.RentItemView.as_view(),name = "rentItem"),
-    path('rentitem/<int:pk>/',views.RentItemDetailView.as_view(),name="rentItemDetail"),
+    path('rentlist/',views.RentItemView.as_view(),name = "rentItem"),
+    path('rentlist/<int:pk>/',views.RentItemDetailView.as_view(),name="rentItemDetail"),
 ]
