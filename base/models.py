a390a4809f854db454b83b9a3bb84ab3b1626793
sachin barali <baralisachin5925@gmail.com>
2024-10-08 10:23:22 +0545

    data:added rentlist data
    database modified and added rentlist data successfully

--- base/models.py
+++ base/models.py
@@ -5,7 +5,6 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 #model for category
 class Categories(models.Model):
     category_name = models.CharField(max_length=100, null=True, blank=True)
-    category_image = models.ImageField(upload_to='categoryIcons',null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -22,7 +21,8 @@ class Rules(models.Model):
 # model for rentitem
 class RentItem(models.Model):
     title = models.CharField(max_length=100,null=True, blank=True)
-    price = models.DecimalField(max_digits=4,decimal_places=2, null=True, blank=True)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=9,decimal_places=2, null=True, blank=True)
     thumbnailImage = models.ImageField(upload_to='thumbnails',null=True, blank=True)
     description = models.TextField()
     quantity = models.IntegerField()
@@ -30,8 +30,8 @@ class RentItem(models.Model):
     rating = models.DecimalField(default=0, max_digits=2, decimal_places=1, validators=[MinValueValidator(0),MaxValueValidator(5)])
     numOfReviews = models.IntegerField(null=True, blank=True)
     address= models.CharField(max_length=255,null=True, blank=True)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6,null=True, blank=True)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6,null=True, blank=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=2,null=True, blank=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=2,null=True, blank=True)
     rules = models.ManyToManyField(Rules, blank=True)
 
     def __str__(self):
